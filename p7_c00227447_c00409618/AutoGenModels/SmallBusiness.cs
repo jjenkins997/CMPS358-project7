89d4b18804a9c67649dd178f2cfeceddb787b108
Tomas Parker <C00409618@louisiana.edu>
2022-04-01 19:40:15 -0500

    Methodsv1
    -First Method, Method (a) Completed

--- p7_c00227447_c00409618/AutoGenModels/SmallBusiness.cs
+++ p7_c00227447_c00409618/AutoGenModels/SmallBusiness.cs
@@ -27,7 +27,7 @@ namespace p7_c00227447_c00409618
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("Filename=../../../../smallbusiness.db");
+                optionsBuilder.UseSqlite("Filename=smallbusiness.db");
             }
         }
 
--- p7_c00227447_c00409618/Program.cs
+++ p7_c00227447_c00409618/Program.cs
@@ -1,4 +1,25 @@
-﻿static void Main(String[] args)
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Design;
+using p7_c00227447_c00409618;
+
+//Method a) all discontinued products
+listDiscontinued();
+
+static void listDiscontinued()
 {
-    
+    using var db = new SmallBusiness();
+    {
+        var results = from n in db.Products where n.IsDiscontinued.ToString() == "1" select n;
+        if (results.Count() == 0)
+        {
+            Console.WriteLine($"No discontinued products");
+            return;
+        }
+        Console.WriteLine("Discontinued products: ");
+        foreach(var e in results)
+            Console.WriteLine(e.ProductName);
+        Console.WriteLine();
+    }
 }
