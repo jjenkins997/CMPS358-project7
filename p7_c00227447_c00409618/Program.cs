933e431e331e3510d6c89c3685f9c52718e00d11
Tomas Parker <C00409618@louisiana.edu>
2022-04-02 19:33:27 -0500

    Methodsv2
    -Method b) completed

--- p7_c00227447_c00409618/Program.cs
+++ p7_c00227447_c00409618/Program.cs
@@ -7,6 +7,10 @@ using p7_c00227447_c00409618;
 //Method a) all discontinued products
 listDiscontinued();
 
+//Method b) given a country give all names and numbers of customers in that country
+Console.WriteLine("Please Enter a Country: ");
+countryCustomers(Console.ReadLine());
+
 static void listDiscontinued()
 {
     using var db = new SmallBusiness();
@@ -20,6 +24,23 @@ static void listDiscontinued()
         Console.WriteLine("Discontinued products: ");
         foreach(var e in results)
             Console.WriteLine(e.ProductName);
+        Console.WriteLine();
+    }
+}
+
+static void countryCustomers(string country)
+{
+    using var db = new SmallBusiness();
+    {
+        var results = from n in db.Customers where n.Country == country select n;
+        if (results.Count() == 0)
+        {
+            Console.WriteLine($"No Customers in this country");
+            return;
+        }
+        Console.WriteLine("Cusomters in " + country + ":");
+        foreach (var e in results)
+            Console.WriteLine(e.FirstName + " " + e.LastName + " " + e.Phone);
         Console.WriteLine();
     }
 }
